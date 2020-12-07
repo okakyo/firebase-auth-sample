9ec8f28ef413aaf260b08e3112ebf4a13a9a72a9
=okakyo <=0622okakyo@gmail.com>
2020-12-07 10:30:26 +0900

    FirstCommit

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,11 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  name: 'DefaultLayout',
+  setup() {},
+})
 </script>
 <style>
 html {
